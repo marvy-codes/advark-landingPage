928be1350c44b6892c0be5942723e2002fb14433
marvyCodes <olowomarve@gmail.com>
2023-01-09 21:31:20 +0100

    color config

--- src/components/Layout/Header/Header.vue
+++ src/components/Layout/Header/Header.vue
@@ -1,5 +1,16 @@
 <template>
-    <h1>Advark Header</h1>
+    <div class="m-6 flex justify-between">
+        <div>
+            <img src="../../../assets/advark.svg" alt="aardvark"/>
+        </div>
+        <div class="flex mt-2">
+            <div class="space-y-1.5">
+                <span class="block w-5 h-0.5 bg-white "></span>
+                <span class="block w-5 h-0.5 bg-white "></span>
+                <span class="block w-5 h-0.5 bg-white "></span>
+            </div>
+        </div>
+    </div>
 </template>
 
   <script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,8 @@
-
 <template>
   <div>
     <Layout>
       <div class="flex items-center justify-center bg-gray-100">
-    <span class="block text-2xl">TODO: Build Your App</span>
+    <span class="block text-2xl font-pop">TODO: Build Your App</span>
   </div>
     </Layout>
   </div>
