26cc4705cb476cc318cf39905628228160a6d2b5
marvyCodes <olowomarve@gmail.com>
2023-01-12 22:16:14 +0100

    feat: desktop hero section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,15 +10,19 @@
           />
           <img
             src="../assets/background.png"
-            class="absolute top-0 right-0 -z-10  mr-[0px] h-screen"
+            class="absolute top-0 right-0 -z-10 mr-[0px] h-screen"
           />
         </div>
-        <div class=" mx-auto w-[340px] text-center sm:w-[650px] md:w-[700px] lg:w-[1100px] relative lg:pl-10">
-          <div class="hidden lg:block absolute -left-8 top-5">
-            <img src="../assets/pharaohicon.svg" class="w-[122px] h-[161px]" />
+        <div
+          class="relative mx-auto w-[340px] text-center sm:w-[650px] md:w-[700px] lg:w-[1100px] lg:pl-10"
+        >
+          <div class="absolute -left-8 top-5 hidden lg:block">
+            <img src="../assets/pharaohicon.svg" class="h-[161px] w-[122px]" />
           </div>
           <div class="">
-            <h1 class="text-[40px] font-bold leading-[64px] text-white      lg:text-[64px] lg:leading-[104px] md:">
+            <h1
+              class="md: text-[40px] font-bold leading-[64px] text-white lg:text-[64px] lg:leading-[104px]"
+            >
               Experience the
               <span class="mr-[-28px] sm:mr-[0px]"
                 ><img
@@ -39,7 +43,9 @@
             </h1>
           </div>
         </div>
-        <div class="mx-[42px] mt-[32px] text-center text-sm leading-[22px] lg:w-[700px] lg:mx-auto lg:text-base lg:leading-6 lg:font-normal">
+        <div
+          class="mx-[42px] mt-[32px] text-center text-sm leading-[22px] lg:mx-auto lg:w-[700px] lg:text-base lg:font-normal lg:leading-6"
+        >
           Experience secure, private messaging on the blockchain. Join our
           decentralized platform and take control of your online communication.
         </div>
