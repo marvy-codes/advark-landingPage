7ce038793a0697a2f49203b63628a2a594e56cd7
marvyCodes <olowomarve@gmail.com>
2023-02-11 20:17:39 +0100

    char: remove product link

--- src/components/Layout/Header/HeaderView.vue
+++ src/components/Layout/Header/HeaderView.vue
@@ -53,7 +53,7 @@
               </div>
               <div class="ml-[50px] mb-[24px] mt-[32px]">
                 <div class="-translate-x-[26px]">
-                  <a href="/product" class="product__one"
+                  <a href="/" class="product__one"
                     ><i class="bi bi-cpu mr-2"></i>Embedded wallet based
                     messenger
                     <svg
@@ -161,7 +161,7 @@
               </div>
               <div class="] mb-[24px] mt-[32px]">
                 <div class="text-sm font-semibold text-white">
-                  <a href="/product" class="" @click="MenuOpen()"
+                  <a href="/" class="" @click="MenuOpen()"
                     ><i class="bi bi-cpu mr-2"></i>Embedded wallet based
                     messenger
                   </a>
