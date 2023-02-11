8801568b2b1b5a949a5f112a87f95e7bfec7a4ef
marvyCodes <olowomarve@gmail.com>
2023-02-11 23:19:59 +0100

    add: assets

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -95,30 +95,98 @@
           </div>
         </div>
 
-        <div class="md:mt-[60px]">
-            <SubHeading>Compatible platforms</SubHeading>
-            <div
-                class="mx-[42px] mt-[18px] text-center text-sm leading-[22px] lg:mx-auto lg:w-[793px] lg:text-base lg:font-normal lg:leading-6"
-            >
+        <div class="">
+          <h2 class="px-[65px] text-center font-semibold md:font-bold text-[32px] md:text-[42px] leading-[52px] md:leading-[80px] text-[#ffffffde]">
+            Compatible platforms
+          </h2>
+          <div class="mx-[42px] mt-[18px] text-center text-sm leading-[22px] lg:mx-auto lg:w-[793px] lg:text-base lg:font-normal lg:leading-6">
             Our web3 messaging app is designed to integrate seamlessly into a variety of platforms, making it easy for you to communicate anywhere at anytime!
           </div>
         </div>
-
         <!-- CARDS -->
-        <div>
-            <div>
-                
-                <!-- detailed cards -->
-                <div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
+        <div class="mt-[48px] md:mt-[80px]">
+            <div class="bg-[#ffffff0d] rounded-[32px] px-[32px] md:px-[64px] pt-[40px] pb-[67px] border-[#ffffff1a] border-[1px]">
+                <div class="flex items-center justify-center">
+                  <h1 class="custom_text_gradient text-center text-[27px] font-bold leading-[54px] lg:text-[48px] lg:leading-[80px] mr-[15px]">
+                    &lt;Games&gt;
+                  </h1>
+                  <div>
+                    <img src="../assets/pad.png" alt="aadvvark" class="w-[56px] h-[56px]"/>
+                  </div>
+                </div>
+
+                <div class="mx-[42px] mt-[18px] text-center text-sm leading-[22px] lg:mx-auto lg:w-[649px] lg:text-base lg:font-normal lg:leading-6">
+                  Forem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus.
+                </div>
+
+                <div class="mt-[64px] text-center">
+                  <button class="rounded-lg bg-gradient-to-r from-[#db00fb] to-[#ff6000] px-[24px] py-[14px] text-xl font-semibold leading-8 text-[#FFFFFF]">
+                    Get started - <span class="text-base font-extralight leading-[24px]">free trial!</span>
+                   </button>
+                </div>
+
+                <div class="text-[20px] leading-[32px] font-semibold text-white text-center mt-[66px]">Available features</div>
+
+                <!-- detailed sections -->
+                <div class="mt-[20px] md:mt-[32px]">
+                    <div class="flext">
+                      <div class="max-w-[524px] bg-[#ffffff14] border-[#ffffff1a] border-[1px] rounded-[16px] px-[24px] pt-[24px]">
+                        <div class="flex justify-center justify-start">
+                          <div class="mr-[8px]"><img src="../assets/wallet.png" alt="aadvark" /></div>
+                          <h1 class="text-[20px] custom_text_gradient text-left font-semibold">wallet &lt;&gt; wallet messaging</h1>
+                        </div>
+                        <div>Need to enable chat between two wallets in game? We support a number of different message types and you can send messages between Users, Wallets and NFTs, with more coming soon.</div>
+                      </div>
+                      <div>
+
+                      </div>
+                    </div>
                 </div>
             </div>
 
             <div></div>
             <div></div>
+        </div>
+
+
+        <!--  -->
+        <div class="mt-[112px] md:mt[200px]">
+          <h2 class="mx-auto text-center font-semibold md:font-bold text-[32px] md:text-[42px] leading-[52px] md:leading-[80px] text-[#ffffffde] max-w-[564px]">
+            Why use Aardvark Embedded messenger?
+          </h2>
+          <div class="mx-[42px] mt-[18px] text-center text-sm leading-[22px] lg:mx-auto lg:w-[793px] lg:text-base lg:font-normal lg:leading-6">
+            Our web3 messaging app is designed to integrate seamlessly into a variety of platforms, making it easy for you to communicate anywhere at anytime!
+          </div>
+        </div>
+
+        
+        <!--  -->
+        <div class="mt-[112px] md:mt[200px]">
+          <h2 class="mx-auto text-center font-semibold md:font-bold text-[32px] md:text-[42px] leading-[52px] md:leading-[80px] text-[#ffffffde] max-w-[564px]">
+            How does Aardvark Embedded messenger work?
+          </h2>
+          <div class="mx-[42px] mt-[18px] text-center text-sm leading-[22px] lg:mx-auto lg:w-[793px] lg:text-base lg:font-normal lg:leading-6">
+            Aardvark comes out of the box compatible with more than 20 leading blockchains, and we are adding support for more chains every day.
+          </div>
+        </div>
+
+        <!--  -->
+        <div class="mt-[112px] md:mt[200px]">
+          <h2 class="mx-auto text-center font-semibold md:font-bold text-[32px] md:text-[42px] leading-[52px] md:leading-[80px] text-[#ffffffde] max-w-[564px]">
+            Testimonials
+          </h2>
+          <div class="mx-[42px] mt-[18px] text-center text-sm leading-[22px] lg:mx-auto lg:w-[793px] lg:text-base lg:font-normal lg:leading-6">
+            Hear what platforms and individuals using Aardvark embedded messenger have to say!
+          </div>
+
+          <div>
+            <div class="">
+              <img src="../assets/howimage.png" alt="aadvark" class="" />
+            </div>
+            <div>
+
+            </div>
+          </div>
         </div>
       </div>
     </PageLayout>
