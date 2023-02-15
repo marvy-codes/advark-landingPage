b2234b4891a6cc0727489f9c7fbbcbac6a0c2b6b
marvyCodes <olowomarve@gmail.com>
2023-02-15 02:17:50 +0100

    add: secfions

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -192,18 +192,94 @@
             class="mx-auto max-w-[564px] text-center text-[32px] font-semibold leading-[52px] text-[#ffffffde] md:text-[42px] md:font-bold md:leading-[80px]"
           >
             Why use Aardvark Embedded messenger?
-          </h2>
-          <div
-            class="mx-[42px] mt-[18px] text-center text-sm leading-[22px] lg:mx-auto lg:w-[793px] lg:text-base lg:font-normal lg:leading-6"
-          >
-            Our web3 messaging app is designed to integrate seamlessly into a
-            variety of platforms, making it easy for you to communicate anywhere
-            at anytime!
+        </h2>
+        
+          <div class="lg:flex justify-between mt-[64px] lg:mt-[88px]">
+            <div class="mr-6">
+              <h1 class="text-center lg:text-left mx-auto w-[234px] lg:w-[500px] custom_text_gradient text-[24px] lg:text-[48px] leading-[40px] lg:leading-[80px] font-semibold lg:font-bold ">
+                Unparalleled chain compatibility
+              </h1>
+                <div class="mt-[24px] lg:w-[490px]  text-[16px] leading-[24px] font-normal text-center lg:text-left mx-auto ">
+                  Aardvark comes out of the box compatible with more than 20 leading blockchains,
+                  and we are adding support for more chains every day.
+              </div>
+            </div>
+            <div class="lg:mr-16 mb-[40px] lg:mb-[0px] mt-[56px] lg:mt-[0px]">
+              <img
+                src="../assets/why1.png"
+                alt="aadvark"
+                class="scale-90 lg:scale-100  mx-auto ">
+            </div>  
           </div>
+
+          <div class="lg:flex justify-between flex-row-reverse mt-[72px] lg:mt-[150px]">
+            <div class="mr-6">
+              <h1 class="text-center lg:text-left mx-auto w-[234px] lg:w-[500px] custom_text_gradient text-[24px] lg:text-[48px] leading-[40px] lg:leading-[80px] font-semibold lg:font-bold ">
+                Reliable & Secure
+              </h1>
+                <div class="mt-[24px] lg:w-[490px]  text-[16px] leading-[24px] font-normal text-center lg:text-left mx-auto ">
+                  Aardvark has > 99.99% uptime and our cryptographic proofs ensure messages are always delivered to the correct recipient, with no leakage of personal data like other messaging providers. 
+              </div>
+            </div>
+            <div class="lg:mr-16 mb-[40px] lg:mb-[0px] mt-[56px] lg:mt-[0px]">
+              <img
+                src="../assets/why2.png"
+                alt="aadvark"
+                class="scale-90 lg:scale-100  mx-auto hidden md:block">
+                <img
+                src="../assets/why22.png"
+                alt="aadvark"
+                class="mx-auto md:hidden">
+            </div>  
+          </div>
+
+          <div class="lg:flex justify-between mt-[64px] mt-[72px] lg:mt-[150px]">
+            <div class="mr-6">
+              <h1 class="text-center lg:text-left mx-auto w-[234px] lg:w-[500px] custom_text_gradient text-[24px] lg:text-[48px] leading-[40px] lg:leading-[80px] font-semibold lg:font-bold ">
+                SDKs available
+              </h1>
+                <div class="mt-[24px] lg:w-[490px]  text-[16px] leading-[24px] font-normal text-center lg:text-left mx-auto ">
+                  Aardvark offer software development kits (SDKs) that allow developers to build and integrate decentralized messaging features 
+                  into their applications. These SDKs provide a set of tools, libraries, and APIs that simplify the process of creating and sending 
+                  messages, managing identities, and handling transactions on a decentralized network.
+              </div>
+              <div class="w-[136px] mx-auto lg:ml-[0px]">
+                  <a class="why3 mt-[56px] block  rounded-lg bg-white px-[24px] py-[14px] text-xl font-semibold leading-8"
+                   href=""
+                  >
+                  </a>
+              </div>
+            </div>
+            <div class="lg:mr-16 mb-[40px] lg:mb-[0px] mt-[56px] lg:mt-[0px]">
+              <img
+                src="../assets/why3.png"
+                alt="aadvark"
+                class="scale-90 lg:scale-100  mx-auto ">
+            </div>  
+          </div>
+
+          <!-- <div class="lg:flex justify-between items-center  ">
+            <div class="">
+              <h1 class="">Unparalleled chain compatibility</h1>
+                <div class="">
+                  Aardvark comes out of the box compatible with more than 20 leading blockchains,
+                  and we are adding support for more chains every day.
+              </div>
+            </div>
+            <div class="lg:mr-16 mb-[40px] lg:mb-[0px]">
+              <img
+                src="../assets/why1.png"
+                alt="aadvark"
+                class="scale-90 lg:scale-100  mx-auto ">
+            </div>  
+          </div> -->
+
         </div>
 
+
+
         <!--  -->
-        <div class="md:mt[200px] mt-[112px]">
+        <div class="md:mt[240px] mt-[112px]">
           <h2
             class="mx-auto max-w-[650px] text-center text-[32px] font-semibold leading-[52px] text-[#ffffffde] md:text-[42px] md:font-bold md:leading-[80px]"
           >
@@ -223,13 +299,13 @@
                 alt="aadvark"
                 class="scale-90 lg:scale-100  mx-auto ">
               </div>
-            <div class="">
+            <div class="xl:-translate-x-16  2xl:-translate-x-20">
               <ul class="mx-auto">
                 <li
                   class="mb-[24px] flex items-center text-[16px] font-semibold leading-[24px] lg:mb-[10px] xl:mb-[37px] md:text-[20px] md:leading-[32px] lg:max-w-[492px]"
                 >
                   <img
-                    src="../assets/howicon1.png" 3uyasb
+                    src="../assets/howicon1.png"
                     alt="aadvark"
                     class="mr-[32px]"
                   />
@@ -308,6 +384,91 @@
           >
             Hear what platforms and individuals using Aardvark embedded
             messenger have to say!
+          </div>
+
+
+          <div class="mx-auto mt-[40px] flex-wrap justify-center md:mt-[70px] md:flex"> 
+            <div
+              class="m-[16px] rounded-3xl border-[1px] border-[#ffffff1a] bg-[#ffffff0d] p-[24px] text-center md:w-[340px] md:text-left xl:w-[380px]"
+            >
+              <div class="text-sm font-normal leading-6">
+                I and our cryptographic proofs ensure messages are always delivered to the correct recipient, with no leakage of personal data like other messaging providers.
+              </div>
+              <div
+                class="mt-[66px] md:leading-40px mx-auto mb-[28px] flex items-center justify-center text-xl font-semibold text-[#ffffffde] md:text-2xl xl:justify-start"
+              >
+                <img
+                  src="../assets/Consolidatedinbox.png"
+                  alt=""
+                  class="mr-3 md:h-[48px] md:w-[48px]"
+                /><div>
+                  <span>Defi Kingdoms</span><br>
+                  <span class="text-sm leading-[22px] lg:text-base lg:font-normal lg:leading-6">Gaming platform</span>
+                </div>
+              </div>
+            </div>
+            <div
+              class="m-[16px] rounded-3xl border-[1px] border-[#ffffff1a] bg-[#ffffff0d] p-[24px] text-center md:w-[340px] md:text-left xl:w-[380px]"
+            >
+              <div class="text-sm font-normal leading-6">
+                I and our cryptographic proofs ensure messages are always delivered to the correct recipient, with no leakage of personal data like other messaging providers.
+              </div>
+              <div
+                class="mt-[66px] md:leading-40px mx-auto mb-[28px] flex items-center justify-center text-xl font-semibold text-[#ffffffde] md:text-2xl xl:justify-start"
+              >
+                <img
+                  src="../assets/Consolidatedinbox.png"
+                  alt=""
+                  class="mr-3 md:h-[48px] md:w-[48px]"
+                /><div>
+                  <span>Defi Kingdoms</span><br>
+                  <span class="text-sm leading-[22px] lg:text-base lg:font-normal lg:leading-6">Gaming platform</span>
+                </div>
+              </div>
+            </div>
+            <div
+              class="m-[16px] rounded-3xl border-[1px] border-[#ffffff1a] bg-[#ffffff0d] p-[24px] text-center md:w-[340px] md:text-left xl:w-[380px]"
+            >
+              <div class="text-sm font-normal leading-6">
+                I and our cryptographic proofs ensure messages are always delivered to the correct recipient, with no leakage of personal data like other messaging providers.
+              </div>
+              <div
+                class="mt-[66px] md:leading-40px mx-auto mb-[28px] flex items-center justify-center text-xl font-semibold text-[#ffffffde] md:text-2xl xl:justify-start"
+              >
+                <img
+                  src="../assets/Consolidatedinbox.png"
+                  alt=""
+                  class="mr-3 md:h-[48px] md:w-[48px]"
+                /><div>
+                  <span>Defi Kingdoms</span><br>
+                  <span class="text-sm leading-[22px] lg:text-base lg:font-normal lg:leading-6">Gaming platform</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          
+
+          <div class="lg:flex justify-between mt-[64px] mt-[72px] lg:mt-[150px] flex-row-reverse mt-[93px] lg:[200px]">
+            <div class="mr-6">
+              <h1 class="text-center lg:text-left mx-auto w-[234px] lg:w-[500px] text-white text-[24px] lg:text-[48px] leading-[40px] lg:leading-[80px] font-semibold lg:font-bold ">
+                Developer Docs
+              </h1>
+                <div class="mt-[24px] lg:w-[490px]  text-[16px] leading-[24px] font-normal text-center lg:text-left mx-auto ">
+                  Our developer documentation provides a comprehensive guide to the platform's features, tools, and APIs, as well as step-by-step instructions for integrating and using these elements in their projects.
+              </div>
+              <div class="w-[215px] mx-auto lg:ml-[0px]">
+                  <a class="text1 mt-[56px] block  rounded-lg bg-white px-[24px] py-[14px] text-xl font-semibold leading-8"
+                   href=""
+                  >
+                  </a>
+              </div>
+            </div>
+            <div class="lg:mr-16 mb-[40px] lg:mb-[0px] mt-[56px] lg:mt-[0px]">
+              <img
+                src="../assets/test1.png"
+                alt="aadvark"
+                class="scale-90 lg:scale-100  mx-auto ">
+            </div>  
           </div>
         </div>
       </div>
