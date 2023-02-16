17e76da551a0c006ab0d5a874fbc4e8e45cf173f
marvyCodes <olowomarve@gmail.com>
2023-02-16 01:26:48 +0100

    feat: section

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -45,7 +45,7 @@
             </button>
           </div>
 
-          <div class="-translate-y-24">
+          <div class="mx-auto">
             <img src="../assets/producthero.png" alt="aadvark" class="" />
           </div>
         </div>
@@ -77,7 +77,6 @@
               />
             </div>
             <!--  -->
-
             <div class="hidden text-center xl:block">
               <img
                 src="../assets/dthundercorelogo.png"
@@ -95,6 +94,8 @@
           </div>
         </div>
 
+
+        <!-- Compatible platforms -->
         <div class="">
           <h2
             class="px-[65px] text-center text-[32px] font-semibold leading-[52px] text-[#ffffffde] md:text-[42px] md:font-bold md:leading-[80px]"
@@ -110,92 +111,155 @@
           </div>
         </div>
         <!-- CARDS -->
-        <div class="mt-[48px] md:mt-[80px]">
-          <div
-            class="rounded-[32px] border-[1px] border-[#ffffff1a] bg-[#ffffff0d] px-[32px] pt-[40px] pb-[67px] md:px-[64px]"
-          >
-            <div class="flex items-center justify-center">
-              <h1
-                class="custom_text_gradient mr-[15px] text-center text-[27px] font-bold leading-[54px] lg:text-[48px] lg:leading-[80px]"
-              >
-                &lt;Games&gt;
-              </h1>
-              <div>
+        <div class="rounded-[32px] border-[1px] border-[#ffffff1a] bg-[#ffffff0d] overflow-hidden mt-[48px] md:mt-[80px] relative max-w-[1300px] mx-auto">
+          <div class="md:flex justify-between items-center lg:items-start">
+            <div class="mt-[71px] md:ml-[64px] text-center md:text-left">
+              <div class="block mx-auto md:hidden">
                 <img
-                  src="../assets/pad.png"
-                  alt="aadvvark"
-                  class="h-[56px] w-[56px]"
-                />
+                    src="../assets/pad.png"
+                    alt="aadvvark"
+                    class="h-[40px] w-[40px] mx-auto"
+                    />
               </div>
-            </div>
-
-            <div
-              class="mx-[42px] mt-[18px] text-center text-sm leading-[22px] lg:mx-auto lg:w-[649px] lg:text-base lg:font-normal lg:leading-6"
-            >
-              Forem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu
-              turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus
-              nec fringilla accumsan, risus sem sollicitudin lacus.
-            </div>
-
-            <div class="mt-[64px] text-center">
-              <button
-                class="rounded-lg bg-gradient-to-r from-[#db00fb] to-[#ff6000] px-[24px] py-[14px] text-xl font-semibold leading-8 text-[#FFFFFF]"
-              >
-                Get started -
-                <span class="text-base font-extralight leading-[24px]"
-                  >free trial!</span
-                >
-              </button>
-            </div>
-
-            <div
-              class="mt-[66px] text-center text-[20px] font-semibold leading-[32px] text-white"
-            >
-              Available features
-            </div>
-
-            <!-- detailed sections -->
-            <div class="mt-[20px] md:mt-[32px]">
-              <div class="flext">
-                <div
-                  class="max-w-[524px] rounded-[16px] border-[1px] border-[#ffffff1a] bg-[#ffffff14] px-[24px] pt-[24px]"
-                >
-                  <div class="flex justify-start justify-center">
-                    <div class="mr-[8px]">
-                      <img src="../assets/wallet.png" alt="aadvark" />
-                    </div>
-                    <h1
-                      class="custom_text_gradient text-left text-[20px] font-semibold"
-                    >
-                      wallet &lt;&gt; wallet messaging
-                    </h1>
-                  </div>
-                  <div>
-                    Need to enable chat between two wallets in game? We support
-                    a number of different message types and you can send
-                    messages between Users, Wallets and NFTs, with more coming
-                    soon.
-                  </div>
+              <div class="md:flex items-center">
+                <h1 class="custom_text_gradient md:mr-[15px] text-[27px] font-bold leading-[54px] xl:text-[48px] lg:leading-[80px]  md:w-fit " >
+                  &lt;In-Game chat&gt;
+                </h1>
+                <div class="hidden lg:block">
+                  <img
+                    src="../assets/pad.png"
+                    alt="aadvvark"
+                    class="h-[40px] w-[40px] xl:h-[56px] xl:w-[56px]"
+                    />
                 </div>
-                <div></div>
               </div>
+              <div class="px-[16px] sm:px-[0px] mt-[18px] text-sm leading-[22px] mx-auto max-w-[489px] lg:text-base lg:font-normal lg:leading-6">
+                The decentralized nature of Web3 technologies means that users are in complete control of their data, giving them more privacy 
+                and security than ever before. This makes Aardvark messaging an ideal solution for in-game chat, where players need to share 
+                sensitive information and communicate quickly and effectively.
+              </div>
+              <div class="mt-[40px] mb-[52px] md:mb-[0px]">
+                <button class="rounded-lg bg-gradient-to-r from-[#db00fb] to-[#ff6000] px-[20px] lg:px-[24px] py-[12px] lg:py-[14px] text-[16px] lg:text-xl font-semibold leading-[24px] lg:leading-8 text-[#FFFFFF]">
+                  Get started - <span class="text-[14px] lg:text-base font-extralight leading-[21px] lg:leading-[24px]">free trial!</span>
+                </button>
+              </div>
+            </div>
+
+            <div class="">
+              <img src="../assets/ingame.png" alt="aadvark" class=" " />
             </div>
           </div>
 
-          <div></div>
-          <div></div>
+          <div>
+            <img src="../assets/fs1.png" class="absolute hidden lg:block xl:top-[550px] top-[500px] left-[64px] -z-10" />
+          </div>
+
+          <div class="mt-[66px] text-center text-[20px] font-semibold leading-[32px] text-white block md:hidden" >
+              features
+          </div>
+          <!-- detailed sections -->
+            <div class="md:mx-[64px] mb-[70px]">
+              <div class="mt-[20px] md:mt-[32px]  flex flex-wrap justify-center ">
+
+                <div class="flex mx-[24px] my-[20px]">
+                  <div class="text-left max-w-[524px] h-[186px] md:h-[202px] rounded-[16px] border-[1px] border-[#ffffff1a] bg-[#ffffff14] px-[24px] pt-[24px]">
+                    <div class="flex justify-start mb-[12px]">
+                      <div class="mr-[8px]">
+                        <img src="../assets/wallet.png" alt="aadvark" />
+                      </div>
+                      <h1
+                        class="custom_text_gradient text-left text-[20px] font-semibold"
+                      >
+                        wallet &lt;&gt; wallet messaging
+                      </h1>
+                    </div>
+                    <div class="text-[14px] leading-[22px] font-normal">
+                      Need to enable chat between two wallets in game? We support
+                      a number of different message types and you can send
+                      messages between Users, Wallets and NFTs, with more coming
+                      soon.
+                    </div>
+                  </div>
+                </div>
+                
+                <div class="flex mx-[24px] my-[20px]">
+                  <div class="text-left max-w-[524px] h-[186px] md:h-[202px] rounded-[16px] border-[1px] border-[#ffffff1a] bg-[#ffffff14] px-[24px] pt-[24px]">
+                    <div class="flex justify-start mb-[12px]">
+                      <div class="mr-[8px]">
+                        <img src="../assets/wallet.png" alt="aadvark" />
+                      </div>
+                      <h1
+                        class="custom_text_gradient text-left text-[20px] font-semibold"
+                      >
+                        wallet &lt;&gt; wallet messaging
+                      </h1>
+                    </div>
+                    <div class="text-[14px] leading-[22px] font-normal">
+                      Need to enable chat between two wallets in game? We support
+                      a number of different message types and you can send
+                      messages between Users, Wallets and NFTs, with more coming
+                      soon.
+                    </div>
+                  </div>
+                </div>
+
+                <div class="flex mx-[24px] my-[20px]">
+                  <div class="text-left max-w-[524px] h-[186px] md:h-[202px] rounded-[16px] border-[1px] border-[#ffffff1a] bg-[#ffffff14] px-[24px] pt-[24px]">
+                    <div class="flex justify-start mb-[12px]">
+                      <div class="mr-[8px]">
+                        <img src="../assets/wallet.png" alt="aadvark" />
+                      </div>
+                      <h1
+                        class="custom_text_gradient text-left text-[20px] font-semibold"
+                      >
+                        wallet &lt;&gt; wallet messaging
+                      </h1>
+                    </div>
+                    <div class="text-[14px] leading-[22px] font-normal">
+                      Need to enable chat between two wallets in game? We support
+                      a number of different message types and you can send
+                      messages between Users, Wallets and NFTs, with more coming
+                      soon.
+                    </div>
+                  </div>
+                </div>
+
+                <div class="flex mx-[24px] my-[20px]">
+                  <div class="text-left max-w-[524px] h-[186px] md:h-[202px] rounded-[16px] border-[1px] border-[#ffffff1a] bg-[#ffffff14] px-[24px] pt-[24px]">
+                    <div class="flex justify-start mb-[12px]">
+                      <div class="mr-[8px]">
+                        <img src="../assets/wallet.png" alt="aadvark" />
+                      </div>
+                      <h1
+                        class="custom_text_gradient text-left text-[20px] font-semibold"
+                      >
+                        wallet &lt;&gt; wallet messaging
+                      </h1>
+                    </div>
+                    <div class="text-[14px] leading-[22px] font-normal">
+                      Need to enable chat between two wallets in game? We support
+                      a number of different message types and you can send
+                      messages between Users, Wallets and NFTs, with more coming
+                      soon.
+                    </div>
+                  </div>
+                </div>
+            </div>
+            </div>
         </div>
 
-        <!--  -->
+
+
+        <!-- Why use aadvark -->
         <div class="md:mt[200px] mt-[112px]">
           <h2
             class="mx-auto max-w-[564px] text-center text-[32px] font-semibold leading-[52px] text-[#ffffffde] md:text-[42px] md:font-bold md:leading-[80px]"
           >
             Why use Aardvark Embedded messenger?
-        </h2>
+          </h2>
         
           <div class="lg:flex justify-between mt-[64px] lg:mt-[88px]">
-            <div class="mr-6">
+            <div class="mr-6 lg:ml-14">
               <h1 class="text-center lg:text-left mx-auto w-[234px] lg:w-[500px] custom_text_gradient text-[24px] lg:text-[48px] leading-[40px] lg:leading-[80px] font-semibold lg:font-bold ">
                 Unparalleled chain compatibility
               </h1>
@@ -218,10 +282,10 @@
                 Reliable & Secure
               </h1>
                 <div class="mt-[24px] lg:w-[490px]  text-[16px] leading-[24px] font-normal text-center lg:text-left mx-auto ">
-                  Aardvark has > 99.99% uptime and our cryptographic proofs ensure messages are always delivered to the correct recipient, with no leakage of personal data like other messaging providers. 
+                  Aardvark has > 99.99% uptime and our cryptographic proofs ensure messages are always delivered to the correct recipient, with no leakage of personal data like other messaging providers.
               </div>
             </div>
-            <div class="lg:mr-16 mb-[40px] lg:mb-[0px] mt-[56px] lg:mt-[0px]">
+            <div class="lg:mr-16 mb-[40px] lg:mb-[0px] mt-[56px] lg:mt-[0px] lg:ml-14">
               <img
                 src="../assets/why2.png"
                 alt="aadvark"
@@ -234,7 +298,7 @@
           </div>
 
           <div class="lg:flex justify-between mt-[64px] mt-[72px] lg:mt-[150px]">
-            <div class="mr-6">
+            <div class="mr-6 lg:ml-14">
               <h1 class="text-center lg:text-left mx-auto w-[234px] lg:w-[500px] custom_text_gradient text-[24px] lg:text-[48px] leading-[40px] lg:leading-[80px] font-semibold lg:font-bold ">
                 SDKs available
               </h1>
@@ -257,28 +321,10 @@
                 class="scale-90 lg:scale-100  mx-auto ">
             </div>  
           </div>
-
-          <!-- <div class="lg:flex justify-between items-center  ">
-            <div class="">
-              <h1 class="">Unparalleled chain compatibility</h1>
-                <div class="">
-                  Aardvark comes out of the box compatible with more than 20 leading blockchains,
-                  and we are adding support for more chains every day.
-              </div>
-            </div>
-            <div class="lg:mr-16 mb-[40px] lg:mb-[0px]">
-              <img
-                src="../assets/why1.png"
-                alt="aadvark"
-                class="scale-90 lg:scale-100  mx-auto ">
-            </div>  
-          </div> -->
-
         </div>
 
 
-
-        <!--  -->
+        <!-- How aadvark messanger works -->
         <div class="md:mt[240px] mt-[112px]">
           <h2
             class="mx-auto max-w-[650px] text-center text-[32px] font-semibold leading-[52px] text-[#ffffffde] md:text-[42px] md:font-bold md:leading-[80px]"
@@ -293,7 +339,7 @@
           </div>
 
           <div class="md:mt[80px] mt-[56px] lg:flex justify-between items-center  ">
-            <div class="lg:mr-16 mb-[40px] lg:mb-[0px]">
+            <div class="lg:mr-16 mb-[40px] lg:mb-[0px] lg:ml-14">
               <img
                 src="../assets/howimage.png"
                 alt="aadvark"
@@ -372,7 +418,8 @@
           </div>
         </div>
 
-        <!--  -->
+
+        <!-- Testimonials -->
         <div class="md:mt[200px] mt-[112px]">
           <h2
             class="mx-auto max-w-[564px] text-center text-[32px] font-semibold leading-[52px] text-[#ffffffde] md:text-[42px] md:font-bold md:leading-[80px]"
@@ -463,7 +510,7 @@
                   </a>
               </div>
             </div>
-            <div class="lg:mr-16 mb-[40px] lg:mb-[0px] mt-[56px] lg:mt-[0px]">
+            <div class="lg:mr-16 mb-[40px] lg:mb-[0px] mt-[56px] lg:mt-[0px] lg:ml-14">
               <img
                 src="../assets/test1.png"
                 alt="aadvark"
